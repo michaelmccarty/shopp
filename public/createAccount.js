c5caa1deb12625b3e7b47ce91a54939faa1155b6
michaelmccarty <mccartyatl@gmail.com>
2019-09-07 12:25:26 -0400

    Fixed passport Local redirect re routing

--- public/createAccount.js
+++ public/createAccount.js
@@ -1,19 +1,19 @@
-// When user clicks add-btn
-$("#loginOrCreate").on("click", function (event) {
-    event.preventDefault();
+// // When user clicks add-btn
+// $("#loginOrCreate").on("click", function (event) {
+//     event.preventDefault();
 
-    // Make a newUser object
-    let newUser = {
-        username: $("#name").val().trim(),
-        password: $("#password").val()
-    };
+//     // Make a newUser object
+//     let newUser = {
+//         username: $("#name").val().trim(),
+//         password: $("#password").val()
+//     };
 
+//     $.post("/login", newUser)
+//         .then(function (data) {
+//             window.location.href = "/main";
+//         });
 
-    $.post("/login", newUser)
-        .then(function (data) {
-        });
-
-    // Empty each input box by replacing the value with an empty string
-    $("#name").val("");
-    $("#password").val("");
-});
+//     // Empty each input box by replacing the value with an empty string
+//     $("#name").val("");
+//     $("#password").val("");
+// });
